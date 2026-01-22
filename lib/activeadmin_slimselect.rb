61cbf097647005c5fc89cd7c817396643792578f
Piers Chambers <piers@varyonic.com>
2026-01-22 10:02:32 -0500

    bundle gem activeadmin_slimselect

---
+++ lib/activeadmin_slimselect.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "activeadmin_slimselect/version"
+
+module ActiveadminSlimselect
+  class Error < StandardError; end
+  # Your code goes here...
+end
