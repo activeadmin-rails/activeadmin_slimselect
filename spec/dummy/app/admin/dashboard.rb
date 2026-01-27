09008865c9bd6c2fea4636c26bfb58e68c210432
Piers Chambers <piers@varyonic.com>
2026-01-27 16:28:11 -0500

    Add dashboard to dummy app to make more usable.

---
+++ spec/dummy/app/admin/dashboard.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register_page "Dashboard" do
+  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+
+  content title: proc { I18n.t("active_admin.dashboard") } do
+    columns do
+      column do
+        panel "Welcome" do
+          para "Welcome to the ActiveAdmin SlimSelect test app."
+        end
+      end
+    end
+  end
+end
